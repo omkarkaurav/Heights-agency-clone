dc2d8606abe429bf7b7299c2ca0c362dd6aea9d3
omkarkaurav <omkarkaurav41@gmail.com>
2024-10-06 15:48:27 +0530

    omkar

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-// var dropdown = gsap.timeline()
+var dropdown = gsap.timeline()
 
-// dropdown.from("nav div",{
-//     y :60,
-//     delay:1,
-//     duration:1,
-//     stagger:0.5
+
+dropdown.from("nav div", {
+    y :-60,
+    duration:0.8,
+    stagger:0.2,
+})
+
+dropdown.from(" .main-video, video",{
+    duration:0.8,
+    opacity:0,
+})
+
+
+dropdown.from(".section-1-text ",{
+    x:-300,
+    duration:0.5,
+    opacity:0,
+    scrub:0.1
+})
+
   
 
-// })
 
-// dropdown.from("nav .part-2 a",{
-//     y:60,
-//     duration:1,
-//     // opacity:0.5,
-//     stagger:0.2
+dropdown.from("#section-2 div",{
+    scrollTrigger:{
+        trigger:"#section-2 div",
+        scroll:".section-1-text",
+        start:"top 120%",
+        end:"top -10%",
+        scrub:1,
+        // markers:true,
+    },
+    x:-300,
+    duration:5,
+    opacity:0,
+    stagger:1,
+    scrub:1,
+    
+})
 
-// })
+
