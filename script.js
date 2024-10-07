611728b7ff1d999b1be35d7e5766d6d3e90747a2
omkarkaurav <omkarkaurav41@gmail.com>
2024-10-08 01:45:31 +0530

    omkar

--- script.js
+++ script.js
@@ -12,34 +12,49 @@ dropdown.from(" .main-video",{
     opacity:0,
 })
 
-
 dropdown.from(".section-1-text ",{
     x:-300,
     duration:0.5,
     opacity:0,
     scrub:0.1
-})
+}) 
 
-  
-
-
-dropdown.from("#section-2 div",{
+gsap.from("#section-2 .section-2-top-right",{
+    y:"-100%",
     scrollTrigger:{
-        trigger:"#section-2 div",
-        scroll:".section-1-text",
-        start:"top 120%",
+        trigger:"#section-2 ",
+        scroll:"body",
+        start:"top 100%",
         end:"top -10%",
         scrub:1,
-        // markers:true,
-    },
-    x:-300,
-    duration:5,
-    opacity:0,
-    stagger:1,
-    scrub:1,
-    
+        stagger:1
+    }
 })
 
+dropdown.from("#section-2 .bottom-top p",{
+    y:"-100%",
+    scrollTrigger:{
+        trigger:"#section-2",
+        scroll:"body",
+        start:"top 90%",
+        end:"top -10%",
+        scrub:1,
+        stagger:1
+    }
+})
+
+dropdown.from("#section-2 .combine",{
+    y:"-100%",
+    markers:true,
+    scrollTrigger:{
+        trigger:"#section-2 ",
+        scroll:"body",
+        start:"top 50%",
+        end:"top -10%",
+        scrub:1,
+        stagger:2
+    }
+})
 
 gsap.to(".boxes-4",{
     y:"-100%",
@@ -53,9 +68,10 @@ gsap.to(".boxes-4",{
         stagger:2
     }
 })
+
 gsap.to(".boxes-3",{
     y:"100%",
-    duration:2,
+    duration:20,
     scrollTrigger:{
         trigger:".pinned-img",
         scroll:"body",
@@ -78,3 +94,52 @@ gsap.to(".pinned-img",{
 })
 
 
+gsap.from("#section-6 .section-6-top-right",{
+    y:"-100%",
+    markers:true,
+    scrollTrigger:{
+        trigger:"#section-6 ",
+        scroll:"body",
+        start:"top 100%",
+        end:"top -10%",
+        scrub:2,
+    }
+})
+
+gsap.from("#section-6 .section-6-middle p",{
+    y:"-100%",
+    markers:true,
+    scrollTrigger:{
+        trigger:"#section-6 ",
+        scroll:"body",
+        start:"top 80%",
+        end:"top -10%",
+        scrub:2,
+    }
+})
+
+
+gsap.from("#section-6 .s-6-bottom-top p",{
+    y:"-100%",
+    markers:true,
+    scrollTrigger:{
+        trigger:"#section-6 ",
+        scroll:"body",
+        start:"top 40%",
+        end:"top -10%",
+        scrub:2,
+    }
+})
+
+
+gsap.from("#section-6 .s-6-bottom-bottom p",{
+    y:"-100%",
+    markers:true,
+    scrollTrigger:{
+        trigger:"#section-6 ",
+        scroll:"body",
+        start:"top 0%",
+        end:"top -10%",
+        scrub:6,
+    }
+})
